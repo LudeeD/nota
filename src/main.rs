e8aa44513ab72206e811be1c8971853faf10c3a8
LudeeD <lfssilva@ua.pt>
2020-04-01 19:52:45 +0100

    initial commit with code

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+fn main() {
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Same as previous examples...
+}
