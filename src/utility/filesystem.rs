3a43312f5314eb77dd3723b40593293ebab859e8
LudeeD <lfssilva@ua.pt>
2020-04-08 18:01:00 +0100

    Add byte reading and writing to filesystem module

--- src/utility/filesystem.rs
+++ src/utility/filesystem.rs
@@ -1,26 +1,27 @@
-use std::fs;
-use std::io;
+use std::path::{PathBuf};
+use std::fs::{read, File, create_dir};
+use std::io::prelude::*;
+use std::io::{Error, BufReader, BufRead};
 
-pub fn create_folder(path: &str) -> Result<(),io::Error>{
-    debug!("create_folder {}", path);
-    let _ = match fs::create_dir(path) {
-        Ok(()) => return Ok(()),
-        Err(error) => return Err(error)
-    };
+pub fn create_folder(path: &PathBuf) -> Result<(),Error>{
+    debug!("create_folder {:?}", path);
+    create_dir(path)
 }
 
-pub fn create_file(path: &str) -> Result<(),io::Error>{
-    debug!("create_file {}", path);
-    let _ = match fs::File::create(path) {
-        Ok(_) => return Ok(()),
-        Err(error) => return Err(error)
-    };   
+pub fn create_file(path: &PathBuf) -> Result<(),Error>{
+    debug!("create_file {:?}", path);
+    match File::create(path) {
+        Ok(_) => Ok(()),
+        Err(error) => Err(error)
+    }
 }
 
-pub fn write() {
-    println!("write")
+
+pub fn read_bytes(path: & PathBuf) -> Result<Vec<u8>, Error> {
+    read(path)
 }
 
-pub fn read() {
-    println!("read")
+pub fn write_bytes(path: & PathBuf, bytes: &Vec<u8>) -> Result<(), Error>{
+    let mut f = File::create(path).expect("Demo");
+    f.write_all(bytes)
 }
