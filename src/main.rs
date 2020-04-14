e2e864dac9efe80973a2e01c849dede88927e44f
LudeeD <lfssilva@ua.pt>
2020-04-14 19:24:54 +0100

    add templates

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,85 @@
 #[macro_use]
-extern crate clap;
-use clap::App;
-
-#[macro_use]
 extern crate log;
 extern crate simple_logger;
 
 #[macro_use]
 extern crate serde;
+use serde::Serialize;
+use serde_json::json;
 
 extern crate bincode;
+
+use warp::Filter;
 
 
 mod application;
 mod service;
 mod utility;
 
-fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+extern crate handlebars;
+use handlebars::{
+    to_json, Context, Handlebars, Helper, JsonRender, Output, RenderContext, RenderError,
+};
 
-    match matches.occurrences_of("verbose") {
-        1 => { println!("Log Level: Error"); simple_logger::init_with_level(log::Level::Error).unwrap();},
-        2 => { println!("Log Level: Warn" ); simple_logger::init_with_level(log::Level::Warn).unwrap(); },
-        3 => { println!("Log Level: Info" ); simple_logger::init_with_level(log::Level::Info).unwrap(); },
-        4 => { println!("Log Level: Debug"); simple_logger::init_with_level(log::Level::Debug).unwrap(); },
-        5 => { println!("Log Level: Trace"); simple_logger::init_with_level(log::Level::Trace).unwrap(); },
-        _ => { println!("Log Level: Info" ); simple_logger::init_with_level(log::Level::Info).unwrap(); }
-    }
-    debug!("Log initialized");
 
-    debug!("Envs loaded");
+use std::error::Error;
+use std::fs::File;
+use std::io::{Read, Write};
 
-    if let Some(matches_init) = matches.subcommand_matches("init") {
-        application::plumbing::init_nota_folder();
-    }
 
-    if let Some(matches_new) = matches.subcommand_matches("new") {
-        let name = matches_new.value_of("NAME").unwrap();
-        application::plumbing::add_nota("demo")
+use std::sync::Arc;
 
-    }
+struct WithTemplate<T: Serialize> {
+    name: &'static str,
+    value: T,
+}
 
+fn render<T>(template: WithTemplate<T>, hbs: Arc<Handlebars>) -> impl warp::Reply
+where
+    T: Serialize,
+{
+    let render = hbs
+        .render(template.name, &template.value)
+        .unwrap_or_else(|err| err.to_string());
+    warp::reply::html(render)
+}
+
+#[tokio::main]
+async fn main() {
+
+    simple_logger::init_with_level(log::Level::Warn).unwrap();
+
+    let mut hb = Handlebars::new();
+
+    hb.register_template_file("title", &"./frontend/title.hbs");
+
+    hb.register_template_file("template.html", &"./frontend/index.hbs")
+        .unwrap();
+
+    // Turn Handlebars instance into a Filter so we can combine it
+    // easily with others...
+    let hb = Arc::new(hb);
+
+    // Create a reusable closure to render template
+    let handlebars = move |with_template| render(with_template, hb.clone());
+
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    let assets = warp::path("assets")
+        .and(warp::fs::dir("./frontend/assets"));
+
+    //GET /
+    let route = warp::get()
+        .and(warp::path::end())
+        .map(|| WithTemplate {
+            name: "template.html",
+            value: json!({"user" : "demo"}),
+        })
+        .map(handlebars);
+
+    let app = hello.or(assets).or(route);
+
+    warp::serve(app).run(([127, 0, 0, 1], 3030)).await;
 }
